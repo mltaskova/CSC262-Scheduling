dc897bfbe6b734850d25a7e3d0e053258883cb24
John Foley <jfoley@cs.umass.edu>
2019-02-26 18:01:17 -0500

    ideal-scheduling harness

---
+++ src/main/java/scheduling/Main.java
@@ -0,0 +1,12 @@
+package scheduling;
+
+public class Main {
+  public static void main(String[] args) {
+    String input_path = "fig_7_1.jobs";
+    if (args.length == 1) {
+      input_path = args[0];
+    }
+
+    System.out.println("scheduling.Main");
+  }
+}
